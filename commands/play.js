d020854ebc68906e73c7de883863c4d8114bcaaf
Kali Vector <53594347+kaliDFDev@users.noreply.github.com>
2020-09-11 13:00:59 +0530

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -788,20 +788,6 @@ module.exports = {
         });
       }
     }
-    if (!serverQueue) {
-      message.member.voice.channel.leave();
-      message.channel.send({
-        embed: {
-          color: message.client.messageEmbedData.color,
-          author: {
-            name: "⭕ Bye, i left your voice channel",
-          },
-          timestamp: new Date(),
-          footer: {
-            text: "© Britta",
-          }
-        }
-      });
     // if (message.client.queue.songs.length > 0) {
     //   play(queue, voiceChannel, message, dispatcher);
     // }
